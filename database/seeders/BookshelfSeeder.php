4b0bbcbf7a29e26d1ec9e23c3545d3699af72ac9
anggarareijnders <anggarapradivta6@gmail.com>
2024-10-26 09:47:47 +0700

    pertemuan ke-4

---
+++ database/seeders/BookshelfSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bookshelf;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BookshelfSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('bookshelves')->insert([
+            [
+                'code'=>'RAK01',
+                'name'=>'Informatika',
+            ], [
+                'code'=>'RAK02',
+                'name'=>'Sipil',
+            ], [
+                'code'=>'GTA03',
+                'name'=>'Filsafat',
+            ]
+        ]);
+
+        //ORM
+        Bookshelf::create([
+            'code'=>'RAK03',
+            'name'=>'Industri',
+        ]);
+    }
+}
