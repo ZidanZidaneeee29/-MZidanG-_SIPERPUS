a05be40c92ca626e0b335f9bdde7e58c1f062b30
ZidanZidaneeee29 <zidanzidane2922@gmail.com>
2024-11-09 09:42:26 +0700

    spatie

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'pustakawan',
+            'email' => 'pustakawan@unsur.ac.id',
+        ])
+        ->assignRole('pustakawan')
+        ->givePermissionTo('edit_book', 'edit_user');
+
+        User::factory()->create([
+            'name' => 'mahasiswa',
+            'email' => 'mahasiswa@unsur.ac.id',
+        ])
+        ->assignRole('mahasiswa')
+        ->givePermissionTo('view_book');
+    }
+}
---
+++ database/seeders/generalseeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use
+class generalseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
